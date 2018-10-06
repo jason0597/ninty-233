781613091f2351b4676224836c998d4d621fdff9
Iason Papadopoulos <jason099080@hotmail.com>
2018-10-06 19:58:53 +0100

    Add Makefile and SHA256 function from WjCryptLib

---
+++ src/sha256/sha256.hpp
@@ -0,0 +1,12 @@
+#ifndef NINTY_233_SHA256_HPP
+#define NINTY_233_SHA256_HPP
+
+#ifndef __cplusplus
+#error Do not include this header in a C project
+#endif
+
+extern "C" {
+#include "sha256.h"
+}
+
+#endif
